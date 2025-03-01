75df6a012abd66605257f31991d096867ff63bea
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-03-02 03:07:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const student = {
   name: "John",
 };
 
-
 function getKeys(student) {
-  return Object.keys(student);
+	// return Object.keys(student);
 	// console.log(Object.keys(student))
+	let arr = [];
+	for(let [key,value] of student){
+		arr.push(key);
+	}
+	return arr
 }
 
 console.log(getKeys(student))
