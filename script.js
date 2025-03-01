4ae7bbdf5a936d4e4ae6e6fe97be3d6eba6d30e0
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-03-02 03:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getKeys(student) {
 	// return Object.keys(student);
 	// console.log(Object.keys(student))
 	let arr = [];
-	for(let [key,value] of student){
+	for(let key in student){
 		arr.push(key);
 	}
 	return arr
